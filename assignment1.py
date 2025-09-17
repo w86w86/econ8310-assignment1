39c515d399c60beae3f255233599573539e33df9
w86w86 <54444342+w86w86@users.noreply.github.com>
2025-09-17 01:45:39 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,5 @@
+
+
 file = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
 #Import statements
 import pandas as pd
@@ -9,6 +11,7 @@ trip = pd.read_csv(file)
 
 # Keep only the dates and the y value
 trip = trip[['Timestamp','trips']]
+print('->trip.Timestamp (before) dtype: ', trip.Timestamp.dtype)
 
 # Format the date (Timestamp
 trip.Timestamp = pd.to_datetime(trip.Timestamp)
@@ -16,31 +19,18 @@ trip.Timestamp = pd.to_datetime(trip.Timestamp)
 # Prophet format using column ds and y
 trip=trip.rename(columns={'Timestamp':'ds','trips':'y'})
 
-"""###Graphic Representation of all trips (hourly)"""
+#Create timeline 31 days
+future = modelFit.make_future_dataframe(periods=744, freq='h', include_history=False)
+print('future.shape: ', future.shape)
 
-import plotly.express as px
-fig = px.line(trip, x='ds', y='y')
-fig.show()
-
-"""###Initialize GAM/Prophet instance and fit to data / modelFit"""
-
-# Commented out IPython magic to ensure Python compatibility.
-# %%time
-# modelFit = Prophet()
-# modelFit.fit(trip)
-
-"""###Create timeline 31 days"""
-
-model = modelFit.make_future_dataframe(periods=744, freq='h', include_history=False)
-
-forecast = modelFit.predict(model)
+forecast = modelFit.predict(future)
+#forecast
 pred = forecast.yhat.tolist()
 
-"""###Dealing with the negative values """
+#Dealing with the negative values
 negative_pred = [(ind, val) \
                  for ind, val in enumerate(pred) if val<0]
+print ('Nb of negative values:', len(negative_pred))
 
-"""####Replace the negative value with min of trip in an hour from our data"""
-
+#Replace the negative value with min of trip in an hour from our data
 pred = [max(min(trip.y), val) for val in pred]
-
